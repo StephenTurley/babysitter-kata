abe779960f85e95e29fa0a238396be1e94bfd398
Stephen Turley <turley714@gmail.com>
2016-06-13 00:19:29 -0400

    fixed some typos, extracted a function

--- spec/babysitter_kata/billing_spec.clj
+++ spec/babysitter_kata/billing_spec.clj
@@ -4,39 +4,40 @@
     [babysitter-kata.billing :as underTest]))
 
 (describe "calculate price"
-	(context "invalid time"
-		(it "should return zero when start time is too early"
-			(should= 0 (underTest/calculat-price "3:00PM" "8:00PM" "1:00AM")))
-		(it "should return zero when end time is too late"
-			(should= 0 (underTest/calculat-price "5:00PM" "8:00PM" "5:00AM"))))
-	(context "billable time"
-		(it "should charge $12 for one hour before bedtime"
-			(should= 12 (underTest/calculat-price "5:00PM" "8:00PM" "6:00PM")))))
+  (context "invalid time"
+    (it "should return zero when start time is too early"
+        (should= 0 (underTest/calculate-price "3:00PM" "8:00PM" "1:00AM")))
+    (it "should return zero when end time is too late"
+        (should= 0 (underTest/calculate-price "5:00PM" "8:00PM" "5:00AM"))))
+  (context "billable time"
+    (it "should charge $12 for one hour before bedtime"
+        (should= 12 (underTest/calculate-price "5:00PM" "8:00PM" "6:00PM")))))
 
 (describe "before?"
-	(it "should return true when time is before"
-		(should (underTest/before? "5:00PM" "6:00PM")))
-	(it "should return true when start is PM and end is AM"
-		(should (underTest/before? "5:00PM" "4:00AM"))))
+  (it "should return true when time is before"
+      (should (underTest/before? "5:00PM" "6:00PM")))
+  (it "should return true when start is PM and end is AM"
+      (should (underTest/before? "5:00PM" "4:00AM"))))
 
 (describe "after?"
-	(it "should return true when time is after"
-		(should (underTest/after? "6:00PM" "5:00PM")))
-	(it "should return true when start is PM and end is AM"
-		(should (underTest/after? "5:00AM" "4:00PM"))))
+  (it "should return true when time is after"
+      (should (underTest/after? "6:00PM" "5:00PM")))
+  (it "should return true when start is PM and end is AM"
+      (should (underTest/after? "5:00AM" "4:00PM"))))
 
 (describe "to-hour"
-	(it "return the hour as a number"
-		(should= 5 (underTest/to-hour "5:00PM"))
-		(should= 10 (underTest/to-hour "10:00PM"))))
+  (it "return the hour as a number"
+    (should= 5 (underTest/to-hour "5:00PM"))
+    (should= 10 (underTest/to-hour "10:00PM"))))
 
-(describe "moring?"
-	(it "should return true if the time contains AM"
-		(should (underTest/morning? "4:00AM"))
-		(should-not (underTest/morning? "4:00PM"))))
+(describe "morning?"
+  (it "should return true if the time contains AM"
+    (should (underTest/morning? "4:00AM"))
+    (should-not (underTest/morning? "4:00PM"))))
 
-(describe "moring?"
-	(it "should return true if the time contains PM"
-		(should (underTest/evening? "4:00PM"))
-		(should-not (underTest/evening? "4:00AM"))))
+(describe "evening?"
+  (it "should return true if the time contains PM"
+    (should (underTest/evening? "4:00PM"))
+    (should-not (underTest/evening? "4:00AM"))))
+
 (run-specs)
--- src/babysitter_kata/billing.clj
+++ src/babysitter_kata/billing.clj
@@ -4,29 +4,33 @@
 (def latest "4:00AM")
 
 (defn to-hour
-	[time]
+  [time]
   (Integer/parseInt (re-find #"^\d{1,2}" time)))
 
 (defn morning?
-	[time]
-	(boolean (re-find #"AM" time)))
+  [time]
+  (boolean (re-find #"AM" time)))
 
 (defn evening?
-	[time]
-	(not (morning? time)))
+  [time]
+  (not (morning? time)))
 
 (defn before?
-	[start end]
-	(or (and (evening? start) (morning? end))
-			(< (to-hour start) (to-hour end))))
+  [start end]
+  (or (and (evening? start) (morning? end))
+      (< (to-hour start) (to-hour end))))
 
 (defn after?
-	[start end]
-	(not (before? start end)))
+  [start end]
+  (not (before? start end)))
 
-(defn calculat-price
-	[start bed end]
-	(if (and
-				(after? start earliest)
-				(before? end latest))
-		12 0))
+(defn valid?
+  [start end]
+  (and
+    (after? start earliest)
+    (before? end latest)))
+
+(defn calculate-price
+  [start bed end]
+  (if (valid? start end)
+    12 0))
